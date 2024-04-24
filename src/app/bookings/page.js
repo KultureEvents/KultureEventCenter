acc0c930691a29b265dd0d98a73bd5cb97c4dbcd
Tobiloba Ibraheem <ibraheemtobiloba15@gmail.com>
2024-04-24 02:53:38 +0100

    booking error fixed

--- src/app/bookings/page.js
+++ src/app/bookings/page.js
@@ -1,58 +1,68 @@
-"use client";
-import React from "react";
-import styles from "./page.module.css";
-import Image from "next/image";
-import { useSearchParams } from "next/navigation";
-import { bookingDetails } from "@/data/data"; // Import the bookingDetails array
+// "use client";
+// import React from "react";
+// import styles from "./page.module.css";
+// import Image from "next/image";
+// import { useSearchParams } from "next/navigation";
+// import { bookingDetails } from "@/data/data"; // Import the bookingDetails array
 
-const BookingsPage = () => {
-  const searchParams = useSearchParams();
-  const selectedPackage = searchParams.get("package");
+// const BookingsPage = () => {
+//   const searchParams = useSearchParams();
+//   const selectedPackage = searchParams.get("package");
 
-  // Find the selected package details from the bookingDetails array
-  const selectedBookingDetails = bookingDetails.find(
-    (packageDetail) => packageDetail.name === selectedPackage
-  );
+//   // Find the selected package details from the bookingDetails array
+//   const selectedBookingDetails = bookingDetails.find(
+//     (packageDetail) => packageDetail.name === selectedPackage
+//   );
 
+//   return (
+//     <section className="section bookings">
+//       <div className="bookings__container container">
+//         <h1 className="title">Bookings</h1>
+//         {selectedPackage ? (
+//           <>
+//             <div>
+//               <h2>{selectedBookingDetails.name} Package Details</h2>
+//               <p>Fee: {selectedBookingDetails.fee}</p>
+//               <Image
+//                 src={selectedBookingDetails.images[0]}
+//                 alt="package"
+//                 width={160}
+//                 height={150}
+//               />
+//               <Image
+//                 src={selectedBookingDetails.images[1]}
+//                 alt="package"
+//                 width={160}
+//                 height={150}
+//               />
+//               <h3>Add-Ons:</h3>
+//               <ul>
+//                 {selectedBookingDetails.addOn.map((addOn, index) => (
+//                   <li key={index}>
+//                     {addOn.star}
+//                     <p>{addOn.text}</p>
+//                     <p>Fee: {addOn.fee}</p>
+//                   </li>
+//                 ))}
+//               </ul>
+//             </div>
+//           </>
+//         ) : (
+//           <p className="subtitle">Please select a package to proceed.</p>
+//         )}
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default BookingsPage;
+
+import React from 'react'
+
+const BookingPage = () => {
   return (
-    <section className="section bookings">
-      <div className="bookings__container container">
-        <h1 className="title">Bookings</h1>
-        {selectedPackage ? (
-          <>
-            <div>
-              <h2>{selectedBookingDetails.name} Package Details</h2>
-              <p>Fee: {selectedBookingDetails.fee}</p>
-              <Image
-                src={selectedBookingDetails.images[0]}
-                alt="package"
-                width={160}
-                height={150}
-              />
-              <Image
-                src={selectedBookingDetails.images[1]}
-                alt="package"
-                width={160}
-                height={150}
-              />
-              <h3>Add-Ons:</h3>
-              <ul>
-                {selectedBookingDetails.addOn.map((addOn, index) => (
-                  <li key={index}>
-                    {addOn.star}
-                    <p>{addOn.text}</p>
-                    <p>Fee: {addOn.fee}</p>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </>
-        ) : (
-          <p className="subtitle">Please select a package to proceed.</p>
-        )}
-      </div>
-    </section>
-  );
-};
+    <div>BookingPage</div>
+  )
+}
 
-export default BookingsPage;
+export default BookingPage
