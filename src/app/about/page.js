3ed0bc1b86caa0ca372b0590cb4e8c453dc8b891
Tobiloba Ibraheem <ibraheemtobiloba15@gmail.com>
2024-05-08 06:42:21 +0100

    testing sanity

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -7,6 +7,8 @@ import AboutHero from "@/appSections/aboutSections/aboutHero/AboutHero";
 import AboutInfo from "@/appSections/aboutSections/aboutInfo/AboutInfo";
 import { client } from "@/sanityClient/sanity";
 
+export const revalidate = 30; // revalidate at most 30 seconds
+
 export const getHomeImage = async () => {
   const groqQuery = `*[_type == "homePage"][0]{
       "teamImages": teamImages[].asset->url
