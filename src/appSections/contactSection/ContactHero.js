e78a15538dac2766124d1050275368623f023550
Tobiloba Ibraheem <ibraheemtobiloba15@gmail.com>
2026-05-25 05:26:03 -0500

    updated the contact info

--- src/appSections/contactSection/ContactHero.js
+++ src/appSections/contactSection/ContactHero.js
@@ -11,23 +11,29 @@ const ContactHero = () => {
         <div className={styles.contactHero__container_contents}>
           <h2>Contact Us</h2>
 
-          <p>
-            <p>ADDRESS:</p>
-            2932 Canton Road Marietta, Georgia 30066
-            <br />
-            <br />
-            <p>CONTACT INFORMATION:</p>
-            info@kultureevents.com <br /> Venue Management <br />
-            Larry Boykins
-            <br /> Tel: 470-257-7997 <br />
-            <br />
-            <p>BUSINESS HOURS:</p>
-            Monday-Sunday 9AM-12AM <br />
-            <br />
-          </p>
-        </div>
+          <div className={styles.contactHero__details}>
+            <p>
+              <strong>ADDRESS:</strong>
+              <br />
+              2932 Canton Road Marietta, Georgia 30066
+            </p>
 
-        <div className={styles.contactHero__container_image}>
+            <p>
+              <strong>CONTACT INFORMATION:</strong>
+              <br />
+              info@kultureevents.com
+              <br />
+              Venue Management
+              <br />
+              Tel: 470-257-7997
+            </p>
+
+            <p>
+              <strong>BUSINESS HOURS:</strong>
+              <br />
+              Monday-Sunday 9AM-12AM
+            </p>
+          </div>
           <Image src={contactHeroImg} alt="about us" width={608} height={568} />
 
           {/* <button
